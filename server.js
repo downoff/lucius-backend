be5c662862a7de769146d26125319e1023d99258
downoff <jerkovic677@gmail.com>
2025-08-06 14:29:49 +0200

    FIX: Implement final production CORS policy

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const corsOptions = {
   origin: function (origin, callback) {
     // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
-    if (whitelist.indexOf(origin) !== --1) {
+    if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
